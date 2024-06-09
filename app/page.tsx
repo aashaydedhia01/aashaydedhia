55f9d73f582620891b98cee281f33c9f68976029
aashaydedia01 <aashaydedia01@gmail.com>
2024-06-09 15:36:09 +0530

    Fixed image import error

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Home from '@/components/Home'
-import About from '@/components/About'
-import Work from '@/components/Work'
-import Wardrobe from '@/components/Wardrobe'
-import Connect from '@/components/Connect'
+// import About from '@/components/About'
+// import Work from '@/components/Work'
+// import Wardrobe from '@/components/Wardrobe'
+// import Connect from '@/components/Connect'
 
 export default function HomePage() {
   return (
